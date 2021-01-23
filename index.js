6f6e4cff4225a9ea2d3624faa3b611e12fb9721b
Felipe Coury <felipe.coury@gmail.com>
2021-01-23 18:26:43 -0300

    feat: added fly.io support

--- index.js
+++ index.js
@@ -9,5 +9,5 @@ require('dotenv').config();
 // });
 
 const server = require('./server');
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8080;
 server.listen(port, () => console.log(`API running on port ${port}`));
